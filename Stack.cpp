6b4c647c1f9317c29ba9db31c47926e72b948ffc
diana-bye <umkadianasad@gmail.com>
2022-04-29 22:15:12 +0300

    Initial commit

---
+++ Stack.cpp
@@ -0,0 +1,70 @@
+#include "Stack.h"
+#include "ListStack.h"
+#include "VectorStack.h"
+#include "StackImplementation.h"
+
+#include <stdexcept>
+
+Stack::Stack(StackContainer container)
+    : _containerType(container)
+{
+    switch (container)
+    {
+    case StackContainer::List: {
+        _pimpl = static_cast<IStackImplementation*>(new ListStack());    // конкретизируйте под ваши конструкторы, если надо
+        break;
+    }
+    case StackContainer::Vector: {
+        _pimpl = static_cast<IStackImplementation*>(new VectorStack());    // конкретизируйте под ваши конструкторы, если надо
+        break;
+    }
+    default:
+        throw std::runtime_error("Неизвестный тип контейнера");
+    }
+}
+
+Stack::Stack(const ValueType* valueArray, const size_t arraySize, StackContainer container)
+{
+    // принцип тот же, что и в прошлом конструкторе
+}
+
+Stack::Stack(const Stack& copyStack)
+{
+    // сами
+}
+
+Stack& Stack::operator=(const Stack& copyStack)
+{
+    // TODO: вставьте здесь оператор return
+}
+
+Stack::~Stack()
+{
+    delete _pimpl;        // композиция!
+}
+
+void Stack::push(const ValueType& value)
+{
+    // можно, т.к. push определен в интерфейсе
+    _pimpl->push(value);
+}
+
+void Stack::pop()
+{
+    _pimpl->pop();
+}
+
+const ValueType& Stack::top() const
+{
+    return _pimpl->top();
+}
+
+bool Stack::isEmpty() const
+{
+    return _pimpl->isEmpty();
+}
+
+size_t Stack::size() const
+{
+    return _pimpl->isEmpty();
+}
